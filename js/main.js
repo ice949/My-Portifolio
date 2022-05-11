42ca78aa4f6a2021c146b3558379f481b32d3298
ice949 <justineimasiku21@gmail.com>
2022-05-11 17:37:45 +0200

    Fix linters

--- js/main.js
+++ js/main.js
@@ -3,90 +3,88 @@ const menu = document.querySelector('.bars');
 const navItems = document.querySelector('.nav-items');
 const closeB = document.querySelector('.closeBtn');
 const navLinks = document.querySelectorAll('.nav-links');
-const box = document.getElementById('box')
+const box = document.getElementById('box');
 
-const popUpBtn = document.getElementById('modal')
+const popUpBtn = document.getElementById('modal');
 
 menu.addEventListener('click', () => {
-    navItems.classList.toggle('nav-bar');
-    closeB.style.display = 'flex';
-    menu.style.display = 'none';
+  navItems.classList.toggle('nav-bar');
+  closeB.style.display = 'flex';
+  menu.style.display = 'none';
 });
 
 function closeBtn() {
-    navItems.classList.remove('nav-bar');
-    menu.style.display = 'block';
+  navItems.classList.remove('nav-bar');
+  menu.style.display = 'block';
 }
 
 navLinks.forEach((element) => {
-    element.addEventListener('click', closeBtn);
+  element.addEventListener('click', closeBtn);
 });
 
 // popup
-const openModalButtons = document.querySelectorAll('[data-modal-target]');
-const closeModalButtons = document.querySelectorAll('[data-close-button]');
-const overlay = document.getElementById('overlay');
 
 const cards = [
-    {   id:1,
-        heading: 'Multi-Post Stories',
-        description: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-        description2 : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  {
+    id: 1,
+    heading: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    description2: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
         when an unknown printer took a galley of type and scrambled it 1960s `,
-        languages: ['css', 'HTML','Bootstrap', 'Ruby'],
-        languages2: ['css', 'HTML', 'Ruby on Rails','Github'],
+    languages: ['css', 'HTML', 'Bootstrap', 'Ruby'],
+    languages2: ['css', 'HTML', 'Ruby on Rails', 'Github'],
 
-        image: "/images/popup.png",
-        seelive: 'See live',
-        seesource: 'See source',
-        button: 'See project'
-    },
-    {
-        id:2,
-        heading: 'Multi-Post Stories',
-        description: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-        description2 : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    image: '/images/popup.png',
+    seelive: 'See live',
+    seesource: 'See source',
+    button: 'See project',
+  },
+  {
+    id: 2,
+    heading: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    description2: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
         when an unknown printer took a galley of type and scrambled it 1960s `,
-        languages: ['css', 'HTML','Bootstrap', 'Ruby'],
-        languages2: ['css', 'HTML', 'Ruby on Rails','Github'],
+    languages: ['css', 'HTML', 'Bootstrap', 'Ruby'],
+    languages2: ['css', 'HTML', 'Ruby on Rails', 'Github'],
 
-        button: 'See project',
-        image: "/images/popup.png",
-        seelive: 'See live',
-        seesource: 'See source'
-    },
-    {
-        id:3,
-        heading: 'Multi-Post Stories',
-        description: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-        description2 : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    button: 'See project',
+    image: '/images/popup.png',
+    seelive: 'See live',
+    seesource: 'See source',
+  },
+  {
+    id: 3,
+    heading: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    description2: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
         `,
-        languages: ['css', 'HTML','Bootstrap', 'Ruby'],
-        languages2: ['css', 'HTML', 'Ruby on Rails','Github'],
+    languages: ['css', 'HTML', 'Bootstrap', 'Ruby'],
+    languages2: ['css', 'HTML', 'Ruby on Rails', 'Github'],
 
-        image: "/images/popup.png",
+    image: '/images/popup.png',
 
-        button: 'See project',
-        seelive: 'See live',
-        seesource: 'See source'
-    },
-    {
-        id:4,
-        heading: 'Multi-Post Stories',
-        description: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-        description2 : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    button: 'See project',
+    seelive: 'See live',
+    seesource: 'See source',
+  },
+  {
+    id: 4,
+    heading: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    description2: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,.`,
-        languages: ['css', 'HTML','Bootstrap', 'Ruby'],
-        languages2: ['css', 'HTML', 'Ruby on Rails','Github'],
-        image: "/images/popup.png",
-        button: 'See project',
-        seelive: 'See live',
-        seesource: 'See source'
-    }
-]
+    languages: ['css', 'HTML', 'Bootstrap', 'Ruby'],
+    languages2: ['css', 'HTML', 'Ruby on Rails', 'Github'],
+    image: '/images/popup.png',
+    button: 'See project',
+    seelive: 'See live',
+    seesource: 'See source',
+  },
+];
 
 const projecCards = cards
   .map(
@@ -104,21 +102,24 @@ const projecCards = cards
              
             </ul>
         </div>
-        <button data-modal-target="#modal" onclick="modal(${card.id})">See Project</button>
+        <button data-modal-target="#modal" class="openmd" onclick="modal(${card.id})">See Project</button>
     </div>
 </div>
-`
-).join('');
+`,
+  ).join('');
 
-  box.innerHTML += projecCards
+box.innerHTML += projecCards;
 
-  const popUp = (card) => {
-      const temp = document.createElement('template')
-      popUpBtn.innerHTML = '';
-      temp.innerHTML = `
+function closeModal() {
+  popUpBtn.classList.remove('active');
+}
+const popUp = (card) => {
+  const temp = document.createElement('template');
+  popUpBtn.innerHTML = '';
+  temp.innerHTML = `
     <div class="modal-header">
         <h3 class="cd12">${card.heading}</h3>
-      <button data-close-button class="close-button" onclick="closeModal()">&times;</button>
+      <button data-close-button class="close-button">&times;</button>
     </div>
     <img src="${card.image}" alt="popup-img">
     <p class="modal-body">
@@ -131,23 +132,20 @@ const projecCards = cards
     <button>${card.seelive} <i class="fa-solid fa-arrow-up-right-from-square"></i></button>
     <button>${card.seesource} <i class="fa-brands fa-github"></i></button>
     </div>
-      `
-      popUpBtn.classList.add('active')
-      popUpBtn.append(temp.content)
+      `;
 
+  popUpBtn.classList.add('active');
+  popUpBtn.append(temp.content);
+  const closebt = document.querySelector('.close-button');
+  closebt.addEventListener('click', closeModal);
+};
 
-  }
-
-
+/* eslint-disable no-unused-vars */
 const modal = (id) => {
-    cards.find((item) => {
-        if(Number(id) === Number(item.id)){
-            return popUp(item)
-        }
-        return undefined
-    })
-}
-
-function closeModal() {
-    popUpBtn.classList.remove('active')
-}
+  cards.find((item) => {
+    if (Number(id) === Number(item.id)) {
+      return popUp(item);
+    }
+    return undefined;
+  });
+};
